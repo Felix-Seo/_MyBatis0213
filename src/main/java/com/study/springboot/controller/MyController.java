10d96d9bc0d4d9c7d80d354f4c9a2d9043eebb3c
Felix <hawker20@naver.com>
2023-02-13 14:54:12 +0900

    test

---
+++ src/main/java/com/study/springboot/controller/MyController.java
@@ -0,0 +1,32 @@
+package com.study.springboot.controller;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.study.springboot.jdbc.Board;
+import com.study.springboot.jdbc.BoardMapper;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class MyController {
+
+	final BoardMapper boardMapper;
+	
+	@GetMapping("/test1")
+	public void ex1(Model mm) {
+		List<Board> list = boardMapper.findAll();
+		mm.addAttribute("ddd", list);
+	}
+
+	@GetMapping("/test2")
+	public String ex2(Model ss) {
+		List<Board> list = boardMapper.findAll();
+		ss.addAttribute("ddd", list);
+		return "test1";
+	}
+}
